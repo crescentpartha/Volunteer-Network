bac4c6e98858e0c537742a66df25a64ec40231ad
crescentpartha <crescentpartha406@gmail.com>
2022-10-12 14:30:06 +0600

    Solving the issue no #1

--- volunteer-network/src/components/Shared/Header/Header.js
+++ volunteer-network/src/components/Shared/Header/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../../images/logo.png';
 
 const Header = () => {
     return (
         <div>
-            <h2>Header</h2>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/home">
+                        <img src={logo} width={150} height={50} alt="Volunteer Network Logo" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className='ms-auto gap-2 fw-semibold'>
+                            <Nav.Link className='mx-2' as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link className='mx-2' as={Link} to="/donation">Donation</Nav.Link>
+                            <Nav.Link className='mx-2' as={Link} to="/events">Events</Nav.Link>
+                            <Nav.Link className='mx-2' as={Link} to="/blog">Blog</Nav.Link>
+                            <Nav.Link className='mx-2 btn btn-primary text-light' as={Link} to="/register">Register</Nav.Link>
+                            <Nav.Link className='mx-2 btn btn-dark text-light' as={Link} to="/admin">Admin</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
