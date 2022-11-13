3d7399cd43854aa91b1df5c97b5b067048fc665a
crescentpartha <crescentpartha406@gmail.com>
2022-11-14 02:36:25 +0600

    volunteerRegistration link added in the every events card

--- volunteer-network/src/components/Home/EventDetail/EventDetail.js
+++ volunteer-network/src/components/Home/EventDetail/EventDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const EventDetail = ({ event }) => {
     const { title, bannerImg } = event;
@@ -8,7 +9,8 @@ const EventDetail = ({ event }) => {
             <Card className='position-relative border-0 shadow-sm'>
                 <Card.Img variant="top" src={bannerImg} alt={title} />
                 <Card.Body className='bg-primary rounded-bottom position-absolute bottom-0 w-100'>
-                    <Card.Title className='text-light'>{title}</Card.Title>
+                    {/* <Card.Title className='text-light'>{title}</Card.Title> */}
+                    <Link className='text-light text-decoration-none' to='/volunteerRegistration'>{title}</Link>
                 </Card.Body>
             </Card>
         </Col>
