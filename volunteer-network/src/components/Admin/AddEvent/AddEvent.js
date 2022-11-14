a814531bbf35b00f8e9fde1e5174b413b6109fa2
crescentpartha <crescentpartha406@gmail.com>
2022-11-14 14:34:23 +0600

    AddEvent section added

--- volunteer-network/src/components/Admin/AddEvent/AddEvent.js
+++ volunteer-network/src/components/Admin/AddEvent/AddEvent.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import cloudUpload from '../../../images/cloud-upload-outline.png';
+import './AddEvent.css';
 
 const AddEvent = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+
     return (
-        <div className='p-4'>
+        <div className='p-4 border-bottom'>
             <h2 className='text-start'>Add Event</h2>
+            <form className='d-flex flex-column gap-2' onSubmit={handleSubmit(onSubmit)}>
+                <div className='d-flex flex-row flex-wrap gap-2 align-items-top justify-content-between bg-light rounded p-4'>
+                    <div className='' style={{ width: '48%' }}>
+                        <div className='text-start pb-3'>
+                            <label className='fw-semibold pb-2' htmlFor="">Event Title</label> <br />
+                            <input style={{ border: '1px solid #d6d6d6'}} className='d-block w-100 px-2 py-1 rounded' placeholder='Event Title' {...register("event-title", { required: true, maxLength: 20 })} />
+                        </div>
+                        <div className='text-start'>
+                            <label className='fw-semibold pb-2' htmlFor="">Description</label> <br />
+                            <textarea style={{ border: '1px solid #d6d6d6' }} className='d-block w-100 px-2 py-1 rounded' placeholder='Description' type="textarea" rows="5" {...register("description", { required: true, maxLength: 30 })} />
+                        </div>
+                    </div>
+                    <div className='' style={{ width: '48%' }}>
+                        <div className='text-start pb-3'>
+                            <label className='fw-semibold pb-2' htmlFor="">Event Date</label> <br />
+                            <input style={{ border: '1px solid #d6d6d6' }} className='d-block w-100 px-2 py-1 rounded' placeholder='Event Date' type="date" {...register("event-date", { required: true })} />
+                        </div>
+                        <div className='text-start'>
+                            <label className='fw-semibold pb-2' htmlFor="">Banner</label> <br />
+                            <div className='d-flex flex-column gap-2'>
+                                <button className='upload-button p-1 px-3 rounded d-block' disabled style={{ border: '1px solid #0084FF', backgroundColor: '#E8F0FE', width: '' }}>
+                                    <img className='pe-2' src={cloudUpload} width='30px' alt="Cloud Upload" />
+                                    <span style={{ color: '#0084FF' }}>Upload image</span>
+                                </button>
+                                <input style={{ border: '1px solid #d6d6d6' }} className='d-block w-100 px-2 py-1 rounded' placeholder='Photo URL' type="url" {...register("photo-url", { required: true })} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='d-flex align-items-center justify-content-end'>
+                    <input className='d-block btn btn-primary' type="submit" value="Add Event" />
+                </div>
+            </form>
         </div>
     );
 };
--- volunteer-network/src/components/Admin/VolunteerRegisterList/VolunteerRegisterList.js
+++ volunteer-network/src/components/Admin/VolunteerRegisterList/VolunteerRegisterList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VolunteerRegisterList = () => {
     return (
-        <div className='p-4'>
+        <div className='p-4 border-bottom'>
             <h2 className='text-start'>Volunteer Register List</h2>
         </div>
     );
