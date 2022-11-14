d3bcd5912bf923d82d9d90c8fc897faf974351ae
crescentpartha <crescentpartha406@gmail.com>
2022-11-15 02:55:43 +0600

    Create a Controlled Inputs Form using React-Bootstrap

--- volunteer-network/src/components/Home/VolunteerRegistration/VolunteerRegistration.js
+++ volunteer-network/src/components/Home/VolunteerRegistration/VolunteerRegistration.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import useLoadSingleEvent from '../../../hooks/useLoadSingleEvent';
 
 const VolunteerRegistration = () => {
-    const {eventDetailId} = useParams();
+    const { eventDetailId } = useParams();
     const [event] = useLoadSingleEvent(eventDetailId);
-    console.log(event);
+    const { title, description } = event;
+    // console.log(event, title);
 
-    const handleRegistration = event => {
-        event.preventDefault();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [date, setDate] = useState('');
+    const [eventDescription, setEventDescription] = useState('');
+    const [eventTitle, setEventTitle] = useState('');
+
+    useEffect( () => {
+        setEventDescription(description);
+        setEventTitle(title);
+    }, [description, title]);
+
+    const handleRegistration = (e) => {
+        e.preventDefault();
+        const registration = { name, email, date, eventDescription, eventTitle };
+
+        console.log(registration);
     }
 
     return (
@@ -17,14 +32,51 @@ const VolunteerRegistration = () => {
             <div style={{ width: '500px' }} className='mt-5 mb-5 mx-auto p-4 border rounded shadow-md'>
                 <h2 className='text-start'>Register as a Volunteer</h2>
                 <Form onSubmit={handleRegistration} className='w-100 mx-auto'>
-                    <Form.Control className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0' type="name" placeholder="Full Name" required />
-                    <Form.Control className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0' type="email" placeholder="Username or Email" required />
-                    <Form.Control className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0' type="date" placeholder="Date" required />
-                    <Form.Control className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0' type="text" placeholder="Description" required />
-                    <Form.Control className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0' type="text" placeholder="Event Name" required />
-                    <Button className='rounded-0 w-100 fw-semibold my-2' variant="primary" type="submit">
-                        Registration
-                    </Button>
+                    <Form.Control
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        type="name"
+                        placeholder="Full Name"
+                        className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0'
+                        required
+                    />
+                    <Form.Control
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        placeholder="Username or Email"
+                        className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0'
+                        required
+                    />
+                    <Form.Control
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        type="date"
+                        placeholder="Date"
+                        className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0'
+                        required
+                    />
+                    <Form.Control
+                        value={eventDescription}
+                        onChange={(e) => setEventDescription(e.target.value)}
+                        type="text"
+                        placeholder="Description"
+                        className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0'
+                        required
+                    />
+                    <Form.Control
+                        value={eventTitle}
+                        onChange={(e) => setEventTitle(e.target.value)}
+                        type="text"
+                        placeholder="Event Name"
+                        className='my-4 fw-semibold border-top-0 border-start-0 border-end-0 rounded-0 px-0'
+                        required
+                    />
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className='rounded-0 w-100 fw-semibold my-2'
+                    >Registration</Button>
                 </Form>
             </div>
         </div>
