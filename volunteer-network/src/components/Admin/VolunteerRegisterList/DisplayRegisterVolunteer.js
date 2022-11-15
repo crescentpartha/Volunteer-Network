7d534d3179ca00a6c9f89e8d64a0c5c5cbf57b5c
crescentpartha <crescentpartha406@gmail.com>
2022-11-16 01:27:39 +0600

    delete a particular volunteerActivity data in client-side and send to the server-side

--- volunteer-network/src/components/Admin/VolunteerRegisterList/DisplayRegisterVolunteer.js
+++ volunteer-network/src/components/Admin/VolunteerRegisterList/DisplayRegisterVolunteer.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import useDisplayActivities from '../../../hooks/useDisplayActivities';
 
 const DisplayRegisterVolunteer = ({ activity }) => {
-    const { name, email, date, eventTitle, eventDescription } = activity;
+    const { _id: id, name, email, date, eventTitle, eventDescription } = activity;
+    const [activities, setActivities] = useDisplayActivities();
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure?');
+        if (proceed) {
+            console.log('Deleting a volunteerActivity with id = ', id);
+
+            // delete a particular volunteerActivity data in client-side and send to the server-side
+            const url = `http://localhost:5000/activity/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                // console.log(data?.deletedCount);
+                if (data.deletedCount > 0) {
+                    console.log('Deleted');
+                    // remove deleted volunteerActivity from the state in client-side for better user experience
+                    const remaining = activities.filter(activity => activity._id !== id);
+                    setActivities(remaining);
+                }
+            });
+        }
+    }
 
     return (
         <tr className='text-start'>
@@ -12,7 +38,11 @@ const DisplayRegisterVolunteer = ({ activity }) => {
             <td>{date}</td>
             <td>{eventTitle}</td>
             <td>{eventDescription}</td>
-            <td><FontAwesomeIcon className='bg-danger p-2 rounded text-light text-center' icon={faTrashCan} /></td>
+            <td>
+                <button className='btn p-0 border-0' onClick={() => handleDelete(id)}>
+                    <FontAwesomeIcon className='bg-danger p-2 rounded text-light text-center' icon={faTrashCan} />
+                </button>
+            </td>
         </tr>
     );
 };
